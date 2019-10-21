b2aa5c10448dd6ec357157ddca480055f4466f95
annen <annen.des0503@gmail.com>
2019-10-21 20:10:26 +0900

    2019年10月度勉強会資料

---
+++ Answer/Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace Answer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.numericUpDownBeforeTemp = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDownAfterTemp = new System.Windows.Forms.NumericUpDown();
+            this.buttonCalc = new System.Windows.Forms.Button();
+            this.labelBeforeTemp = new System.Windows.Forms.Label();
+            this.labelAfterTemp = new System.Windows.Forms.Label();
+            this.treeViewAnswer = new System.Windows.Forms.TreeView();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownBeforeTemp)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownAfterTemp)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // numericUpDownBeforeTemp
+            // 
+            this.numericUpDownBeforeTemp.Location = new System.Drawing.Point(12, 33);
+            this.numericUpDownBeforeTemp.Name = "numericUpDownBeforeTemp";
+            this.numericUpDownBeforeTemp.Size = new System.Drawing.Size(175, 19);
+            this.numericUpDownBeforeTemp.TabIndex = 0;
+            this.numericUpDownBeforeTemp.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // numericUpDownAfterTemp
+            // 
+            this.numericUpDownAfterTemp.Location = new System.Drawing.Point(222, 33);
+            this.numericUpDownAfterTemp.Name = "numericUpDownAfterTemp";
+            this.numericUpDownAfterTemp.Size = new System.Drawing.Size(175, 19);
+            this.numericUpDownAfterTemp.TabIndex = 0;
+            this.numericUpDownAfterTemp.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // buttonCalc
+            // 
+            this.buttonCalc.Location = new System.Drawing.Point(12, 71);
+            this.buttonCalc.Name = "buttonCalc";
+            this.buttonCalc.Size = new System.Drawing.Size(75, 23);
+            this.buttonCalc.TabIndex = 2;
+            this.buttonCalc.Text = "計算実行";
+            this.buttonCalc.UseVisualStyleBackColor = true;
+            this.buttonCalc.Click += new System.EventHandler(this.buttonCalc_Click);
+            // 
+            // labelBeforeTemp
+            // 
+            this.labelBeforeTemp.AutoSize = true;
+            this.labelBeforeTemp.Location = new System.Drawing.Point(12, 18);
+            this.labelBeforeTemp.Name = "labelBeforeTemp";
+            this.labelBeforeTemp.Size = new System.Drawing.Size(87, 12);
+            this.labelBeforeTemp.TabIndex = 3;
+            this.labelBeforeTemp.Text = "現在の設定温度";
+            // 
+            // labelAfterTemp
+            // 
+            this.labelAfterTemp.AutoSize = true;
+            this.labelAfterTemp.Location = new System.Drawing.Point(220, 18);
+            this.labelAfterTemp.Name = "labelAfterTemp";
+            this.labelAfterTemp.Size = new System.Drawing.Size(81, 12);
+            this.labelAfterTemp.TabIndex = 3;
+            this.labelAfterTemp.Text = "設定したい温度";
+            // 
+            // treeViewAnswer
+            // 
+            this.treeViewAnswer.Location = new System.Drawing.Point(12, 100);
+            this.treeViewAnswer.Name = "treeViewAnswer";
+            this.treeViewAnswer.Size = new System.Drawing.Size(385, 250);
+            this.treeViewAnswer.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(409, 362);
+            this.Controls.Add(this.treeViewAnswer);
+            this.Controls.Add(this.labelAfterTemp);
+            this.Controls.Add(this.labelBeforeTemp);
+            this.Controls.Add(this.buttonCalc);
+            this.Controls.Add(this.numericUpDownAfterTemp);
+            this.Controls.Add(this.numericUpDownBeforeTemp);
+            this.Name = "Form1";
+            this.Text = "Answer";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownBeforeTemp)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownAfterTemp)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NumericUpDown numericUpDownBeforeTemp;
+        private System.Windows.Forms.NumericUpDown numericUpDownAfterTemp;
+        private System.Windows.Forms.Button buttonCalc;
+        private System.Windows.Forms.Label labelBeforeTemp;
+        private System.Windows.Forms.Label labelAfterTemp;
+        private System.Windows.Forms.TreeView treeViewAnswer;
+    }
+}
+
---
+++ Answer/Form1.cs
@@ -0,0 +1,143 @@
+﻿#region usings
+using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+#endregion
+
+namespace Answer
+{
+
+    #region Classes
+    public partial class Form1 : Form
+    {
+        #region Constants
+        /// <summary>
+        ///     最高温度
+        /// </summary>
+        private const int MAX_TEMP = 40;
+
+        /// <summary>
+        ///     最低温度
+        /// </summary>
+        private const int MIN_TEMP = 0;
+        #endregion
+
+        #region Instance readonly fields
+        /// <summary>
+        ///     1回の操作で変更可能な温度
+        /// </summary>
+        private readonly List<int> _changeTempList = new List<int>();
+        #endregion
+
+        #region Instance constructors
+        /// <summary>
+        /// </summary>
+        public Form1()
+        {
+            this.InitializeComponent();
+
+            this._changeTempList.Add(-10);
+            this._changeTempList.Add(-5);
+            this._changeTempList.Add(-3);
+            this._changeTempList.Add(3);
+            this._changeTempList.Add(5);
+            this._changeTempList.Add(10);
+
+            this.numericUpDownBeforeTemp.Maximum = Form1.MAX_TEMP;
+            this.numericUpDownBeforeTemp.Minimum = Form1.MIN_TEMP;
+            this.numericUpDownAfterTemp.Maximum = Form1.MAX_TEMP;
+            this.numericUpDownAfterTemp.Minimum = Form1.MIN_TEMP;
+        }
+        #endregion
+
+        #region Instance methods
+        /// <summary>
+        ///     計算ボタンクリック時のイベント
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void buttonCalc_Click(object sender, EventArgs e)
+        {
+            this.treeViewAnswer.Nodes.Clear();
+
+            // 現在の設定温度
+            var beforeTemp = (int)this.numericUpDownBeforeTemp.Value;
+            // 設定したい温度
+            var afterTemp = (int)this.numericUpDownAfterTemp.Value;
+            this.treeViewAnswer.Nodes.Add(new TreeNode { Text = $"現在の設定温度：{beforeTemp}" + "\n" });
+            this.treeViewAnswer.Nodes.Add(new TreeNode { Text = $"設定したい温度：{afterTemp}" + "\n" });
+
+            // 温度と遷移可否を格納
+            var dpDc = new Dictionary<int, bool>();
+            // 最低温度～最高温度に対しての、遷移可否を表す
+            // 最高温度40、最低温度0なら以下のようになる
+            // [0, false],[1, false],[2, false]...[現在の設定温度, true]...[39, false],[40, false]
+            for (var temp = Form1.MIN_TEMP; temp <= Form1.MAX_TEMP; temp++)
+            {
+                dpDc[temp] = false;
+            }
+            // 現在の設定温度は遷移可能とする
+            dpDc[beforeTemp] = true;
+
+            // 設定したい温度の遷移可否が true になるまでループ
+            // ループした回数イコール、リモコンのボタンを押した回数
+            var loopCnt = 0;
+            while (dpDc[afterTemp] == false)
+            {
+                loopCnt++;
+                if (100 <= loopCnt)
+                {
+                    // ループが100回を超えたら設定不可能と判断する
+                    this.treeViewAnswer.Nodes.Add(new TreeNode { Text = "設定不可能です。" });
+                    return;
+                }
+
+                // 現在の設定温度に、変更可能な温度(_changeTempList)を加算した、変更後の設定温度の一覧
+                var newTempList = new List<int>();
+                // 変更可能な温度(_changeTempList)でループ
+                foreach (var changeTemp in this._changeTempList)
+                {
+                    for (var temp = Form1.MIN_TEMP; temp <= Form1.MAX_TEMP; temp++)
+                    {
+                        if (dpDc[temp] == false)
+                        {
+                            continue;
+                        }
+
+                        var newTemp = temp + changeTemp;
+                        if ((newTemp < Form1.MIN_TEMP) || (Form1.MAX_TEMP < newTemp))
+                        {
+                            // 変更後の設定温度が上下限に収まっていない場合は NG とする
+                            continue;
+                        }
+
+                        newTempList.Add(newTemp);
+                    }
+                }
+
+                var keys = new List<int>(dpDc.Keys);
+                foreach (var temp in keys)
+                {
+                    if (newTempList.Contains(temp))
+                    {
+                        dpDc[temp] = true;
+                    }
+                    else
+                    {
+                        dpDc[temp] = false;
+                    }
+                }
+
+                if (dpDc[afterTemp])
+                {
+                    // 設定したい温度に到達
+                    break;
+                }
+            }
+
+            this.treeViewAnswer.Nodes.Add(new TreeNode { Text = $"操作回数：{loopCnt} 回" });
+        }
+        #endregion
+    }
+    #endregion
+}
